cb602512ec8b0ad9b6ba2980c7a34f4166d36d60
mohammadusama2 <mohammadusama2@hotmail.com>
2022-10-02 00:27:34 +0530

    example

---
+++ research_trial_dir/Count_vect.py
@@ -0,0 +1,22 @@
+from numpy import vectorize
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+corpus = [
+    "zebra apple ball cat",
+    "ball cat dog elephant",
+    "very very unique"
+]
+
+# vectorizer = CountVectorizer()
+# x = vectorizer.fit_transform(corpus)
+# print(x.toarray())
+# print(vectorizer.get_feature_names_out())
+
+max_features = 100
+ngrams = 3
+
+vectorizer = CountVectorizer(max_features=max_features, ngram_range=(1, ngrams))
+x = vectorizer.fit_transform(corpus)
+print(x.toarray())
+print(vectorizer.get_feature_names_out())
