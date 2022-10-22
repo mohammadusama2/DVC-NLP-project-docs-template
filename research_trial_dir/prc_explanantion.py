be74dab24767a29fed0a7c2e2a637fbdd2d819a6
mohammadusama2 <mohammadusama2@hotmail.com>
2022-10-22 17:10:18 +0530

    prc_explanation file updated

--- research_trial_dir/prc_explanantion.py
+++ research_trial_dir/prc_explanantion.py
@@ -1,7 +1,9 @@
 precision, recall, prc_threshold = [0.1,0.2,0.4,0.9, 0.99, 0.22, 0.66], [0.1,0.2,0.4,0.9, 0.99, 0.22, 0.66], [0.1,0.2,0.4,0.9, 0.99, 0.22, 0.66]
 
+nth_point = 3 
 print(list(zip(precision, recall, prc_threshold)))
 print(list(zip(precision, recall, prc_threshold))[::2])
+print(list(zip(precision, recall, prc_threshold))[::nth_point])
 
 # so here we can see that out of many values we are taking values 
 # at every 2 interval.
